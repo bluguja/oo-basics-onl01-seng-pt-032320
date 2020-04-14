ab3563d35b1e4b3c2ee47a2219e6a035da4e83e1
Bala Luguja <bluguja@gmail.com>
2020-04-14 23:27:30 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,28 +1,29 @@
-class Book
-  
-  def initialize (title)
-    @title = tittle
+lass Book
+ 
+  def initialize(title)
+    @title = title
   end
-  
+ 
   def title
     @title
   end
-  
+ 
   def author=(author)
-    @authour = author
+    @author = author
   end
-  
-  def author 
+ 
+  def author
     @author
   end
-  
-  def page_count = (num)
-      @page_count
+ 
+  def page_count=(num)
+    @page_count = num
   end
-  
+ 
   def page_count
     @page_count
   end
-  
+ 
+
   
 end
