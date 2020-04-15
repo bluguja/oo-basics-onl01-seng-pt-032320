4be2cc1301c702658927fc9efd8a615a87270664
Bala Luguja <bluguja@gmail.com>
2020-04-15 00:16:11 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,12 +4,14 @@
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
-  @brand = brand
-  BRANDS << brand unless BRANDS.any? { |b| b == brand }
-end
-  
-  BRANDS=[]
+    @brand = brand
+    if !(BRANDS.include?(@brand))
+      BRANDS << @brand
+    end
+  end
   
   def cobble
     
