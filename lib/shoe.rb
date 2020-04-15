c0fca02e7c0f532e3403ba4438abeda70c7ee7b8
Bala Luguja <bluguja@gmail.com>
2020-04-15 00:24:19 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,24 @@
-# Make your shoe class here!
+
+  #require'pry'
+  class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
+  BRANDS = []
+
+  def initialize(brand)
+    @brand = brand
+    if !(BRANDS.include?(@brand))
+      BRANDS << @brand
+    end
+  end
+
+
+  def cobble
+    self.condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+#binding.pry
+end
+
+
