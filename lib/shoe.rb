01b31523717bc673e452a7487e49eeb911ddd88b
Bala Luguja <bluguja@gmail.com>
2020-04-15 00:18:02 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,5 @@
 
   class Shoe
-    
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
@@ -12,19 +11,13 @@
       BRANDS << @brand
     end
   end
-  
-  def cobble
-    
-    self.condition = "new"
-    
-    puts = "Your shoe is as good as new!"
-  
-  end
 
+
+  def cobble
+    self.condition = "new"
+    puts "Your shoe is as good as new!"
+  end
 
 end
 
-  
-  
-  
-  
+
